ce9c6628e6c992c5def25f2af11be7e359ddc685
Alan Assel <asselalan@gmail.com>
2022-11-25 17:27:54 -0300

    Ultimos detalles

--- js/js.js
+++ js/js.js
@@ -82,3 +82,9 @@ function pantallaCargaNoticias(sectionCargaNoticias){
 
 pantallaCargaNoticias(sectionCargaNoticias);
 
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
